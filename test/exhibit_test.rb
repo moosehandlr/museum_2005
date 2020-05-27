6c491dbb87e408e16e14acc164f1d7b55596a86d
Alex Desjardins <ardesjardins91@gmail.com>
2020-05-27 10:23:26 -0600

    Create exihit test

---
+++ test/exhibit_test.rb
@@ -0,0 +1,19 @@
+require './lib/exhibit'
+require 'minitest/autorun'
+require 'minitest/pride'
+
+class ExhibitTest < Minitest::Test
+
+  def setup
+    @exhibit = Exhibit.new({name: "Gems and Minerals", cost: 0})
+  end
+
+  def test_it_exists
+    assert_instance_of Exhibit, @exhibit
+  end
+
+  def test_exhibit_has_attributes
+    assert_equal "Gems and Minerals", @exhibit.name
+    assert_equal 0, @exhibit.cost
+  end
+end
