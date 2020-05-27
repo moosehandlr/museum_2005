d5c44baa29eb3c33142678776c92904cbb13126f
Alex Desjardins <ardesjardins91@gmail.com>
2020-05-27 10:23:34 -0600

    Create patron test

---
+++ test/patron_test.rb
@@ -0,0 +1,28 @@
+require './lib/patron'
+require 'minitest/autorun'
+require 'minitest/pride'
+
+class PatronTest < Minitest::Test
+
+  def setup
+    @patron_1 = Patron.new("Bob", 20)
+  end
+
+  def test_it_exists
+    assert_instance_of Patron, @patron_1
+  end
+
+  def test_patron_has_attributes
+    assert_equal "Bob", @patron_1.name
+    assert_equal 20, @patron_1.spending_money
+    assert_equal [], @patron_1.interests
+  end
+
+  def test_patron_can_add_interests
+    @patron_1.add_interest("Dead Sea Scrolls")
+    @patron_1.add_interest("Gems and Minerals")
+
+    assert_equal ["Dead Sea Scrolls", "Gems and Minerals"], @patron_1.interests
+  end
+
+end
